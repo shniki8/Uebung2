ebedc5467694b9e4c70d1093e3de47ae7082b7ee
Niklas <menkniklas@gmail.com>
2025-10-27 12:28:49 +0100

    p1 a) commit 1

---
+++ p1/P1_main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P1_main {
+
+
+    public static void main(final String[] args) {
+        int i = 10;
+        boolean b = true;
+        /*Scanner s = new Scanner(System.in);
+        String zeile = new String();
+        zeile = s.nextLine();
+        System.out.println(zeile);
+        s.close(); */
+   
+}
+}
